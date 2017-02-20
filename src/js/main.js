97b1b313f8a3f5d327659d73530ce1596ceed388
apfranzen <adamfranzen@gmail.com>
2017-02-20 15:21:24 -0500

    animated page scroll

--- src/js/main.js
+++ src/js/main.js
@@ -12,14 +12,26 @@ $(document).on('ready', () => {
       $(this).addClass('active')
   });
 
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+
   $('.toggle-info').click(function(){
     console.log('toggle-info hit');
     $(this).text(function(i,old){
         return old=='Less info' ?  'More info' : 'Less info';
     });
   });
-
-
 });
 
 // es6 test
