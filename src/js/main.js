ebe27cd4da4aa4af096e452697b97da6690445c0
apfranzen <adamfranzen@gmail.com>
2017-02-20 14:23:00 -0500

    animated each card

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,16 @@ $(document).on('ready', () => {
   // console.log('sanity check!');
 
   $('#nav li a').click(function() {
+    // document.querySelector('.hello').scrollIntoView({
+    //   behavior: 'smooth'
+    // });
     // console.log('hit');
       var $this = $(this);
       $('li a.active').removeClass('active');
       $(this).addClass('active')
   });
+
+
 });
 
 // es6 test
